16bd15d0d0261fd2f9a84dc3d85ce76cdbf3b50e
desp <despbusiness@gmail.com>
2022-12-31 19:30:49 -0500

    use cdn instead of media, somehow fix downloading

--- src/modules/fetch_attachments_from_message.ts
+++ src/modules/fetch_attachments_from_message.ts
@@ -17,14 +17,14 @@ export async function fetch_attachments_from_message(
 	let array_url: string[] = [];
 	let name: string = "";
 	let messaged: string[] = [];
-    let names: object[] = []
+	let names: object[] = [];
 
 	console.log(
 		messages.forEach(async (message: any) => {
 			messaged = message;
 			id = message["id"] || "";
 			url = JSON.parse(JSON.stringify(message["attachments"]))[0][
-				"proxy_url"
+				"url"
 			].toString();
 			array_url = url
 				.toString()
@@ -32,14 +32,16 @@ export async function fetch_attachments_from_message(
 				.split("/");
 			name = array_url.toString().split(/[, ]+/).pop()?.toString() || "";
 
-            names.push({
-                channel: channel,
-                messaged: message,
-                id: id,
-                url: url,
-                array_url: array_url,
-                name: name,
-            })
+			names.push({
+				channel: channel,
+				messaged: message,
+				id: id,
+				url: url,
+				array_url: array_url,
+				name: name,
+			});
+
+			let done: string[] = [];
 
 			await message_attachment_id(parent_name, messaged, {
 				id: id,
@@ -48,35 +50,47 @@ export async function fetch_attachments_from_message(
 				root_name: await channel["name"],
 			});
 
-            JSON.parse(JSON.stringify(await names).toString()).forEach(async (
-                data: any,
-            ) => {
-                const response = await fetch(data.url, {
-                    method: "GET",
-                    headers: {
-                        authorization: authorization_token,
-                    },
-                });
-                const arrayBuffer = await response.arrayBuffer();
-                const buffer = Buffer.from(arrayBuffer);
-                await write_file(
-                    path.join(
-                        process.cwd() + "/data/channels/",
-                        await sanitize(parent_name),
-                        "/",
-                        await sanitize(await data.channel["name"]),
-                        "/",
-                        await sanitize(await data.channel["id"]),
-                        "/",
-                        "attachments",
-                        "/",
-                        await sanitize(data.id),
-                        "/",
-                        await sanitize(data.name),
-                    ),
-                    buffer,
-                );
-            });
+			function sleep(ms: number | undefined) {
+				return new Promise((resolve) => setTimeout(resolve, ms));
+			}
+
+			await JSON.parse(JSON.stringify(await names).toString()).forEach(
+				async (data: any, index: number) => {
+                    async function bruh() {
+                        const response = await fetch(await data.url, {
+                            method: "GET",
+                            headers: {
+                                authorization: authorization_token,
+                            },
+                        });
+    
+                        //console.log(data.url);
+    
+                        const arrayBuffer = await response.arrayBuffer();
+                        const buffer = Buffer.from(arrayBuffer);
+                        await write_file(
+                            path.join(
+                                process.cwd() + "/data/channels/",
+                                await sanitize(parent_name),
+                                "/",
+                                await sanitize(await data.channel["name"]),
+                                "/",
+                                await sanitize(await data.channel["id"]),
+                                "/",
+                                "attachments",
+                                "/",
+                                await sanitize(data.id),
+                                "/",
+                                await sanitize(data.name),
+                            ),
+                            buffer,
+                        );
+                    }
+
+					await sleep(250 * (index + 1));
+                    await bruh()
+				},
+			);
 		}),
 	);
 
